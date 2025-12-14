639395c77ae9a9939fb2709e36fb5c2fd5063e60
Saad Ur Rehman Baig <saadbaig200@gmail.com>
2025-12-14 11:20:31 +0100

    feat: add integrate_train_weather_data to public API

--- src/dramatiq_task/__init__.py
+++ src/dramatiq_task/__init__.py
@@ -1,9 +1,11 @@
 from .train_tasks import collect_train_data
 from .weather_tasks import collect_weather_data
+from .integration_tasks import integrate_train_weather_data
 from .dramatiq_tasks import full_data_pipeline
 
 __all__ = [
     "collect_train_data", 
     "collect_weather_data",
+    "integrate_train_weather_data",
     "full_data_pipeline",
 ]
